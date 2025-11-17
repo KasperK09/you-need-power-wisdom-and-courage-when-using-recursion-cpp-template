84df1ae29888321cbb3c8f158084f43c7db556a1
Kasper <kamkasper@gmail.com>
2025-11-17 16:45:26 -0600

    added tests

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 int main()
 {
+  //same as example output
   TriangleNumberCalculator calculate;
   cout << calculate.value(1) << endl;
   cout << calculate.value(2) << endl;
--- tests/hello_test.cpp
+++ tests/hello_test.cpp
@@ -5,6 +5,40 @@
 
 #include "../src/hello.hpp"
 
-// TEST_CASE( "it returns Hello World" ) {
-//     REQUIRE( hello() == "Hello World!" );
-// }
+TEST_CASE("TriangleNumberCalculator::value") 
+{
+    TriangleNumberCalculator calc;
+
+    SECTION("Base cases")
+    {
+        REQUIRE(calc.value(0) == 0);
+        REQUIRE(calc.value(1) == 1);
+    }
+
+    SECTION("check if triangular numbers calculation works")
+    {
+        REQUIRE(calc.value(1) == 1);
+        REQUIRE(calc.value(2) == 3);
+        REQUIRE(calc.value(3) == 6);
+        REQUIRE(calc.value(4) == 10);
+        REQUIRE(calc.value(5) == 15);
+    }
+}
+
+TEST_CASE("check if TriangleNumberCalculator add works")
+{
+    TriangleNumberCalculator calc;
+
+    REQUIRE(calc.add(1, 1) == 2);
+    REQUIRE(calc.add(2, 3) == 9);
+    REQUIRE(calc.add(4, 2) == 13);
+}
+
+TEST_CASE("check if TriangleNumberCalculator subtract works")
+{
+    TriangleNumberCalculator calc;
+
+    REQUIRE(calc.subtract(1, 1) == 0);
+    REQUIRE(calc.subtract(2, 3) == -3);
+    REQUIRE(calc.subtract(4, 2) == 7);
+}
